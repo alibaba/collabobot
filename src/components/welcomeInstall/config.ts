fcfbb5750f09d31811672f433144ddb1d061c5f4
Frankzhaopku <syzhao1988@126.com>
2019-04-19 17:22:33 +0800

    add default config files.

--- src/components/welcomeInstall/config.ts
+++ src/components/welcomeInstall/config.ts
@@ -5,7 +5,7 @@ export default class WelcomeInstallComponentConfig extends BaseComponentConfig {
         super();
         this.enable = true;
         this.title = "[Notice] Welcome to use collabobot as your repo maintainer!";
-        this.body = "It is my honor to serve this repo, please follow the instructions to properly setup.";
+        this.body = "It is my honor to serve this repo, please check the [default config](https://github.com/alibaba/collabobot/blob/master/configTemplate.js) to properly setup.";
     }
     title: string;
     body: string;
